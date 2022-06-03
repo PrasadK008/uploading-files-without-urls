a8d00607978ef96dcbc4a97b9f1b34140bd75f47
PrasadK008 <56822499+PrasadK008@users.noreply.github.com>
2022-06-03 19:49:22 +0530

    Update uploader.js

--- components/uploader.js
+++ components/uploader.js
@@ -3,23 +3,10 @@ import axios from 'axios';
 
 class Uploader extends Component {
   state = {
-    imageUrl: 'https://placeimg.com/320/320/animals' 
   }
   
   handleUploadFile = (event) => {
-    const data = new FormData()
-    data.append('file', event.target.files[0])
-    data.append('name', 'some value user types')
-    data.append('description', 'some value user types')
-    axios.post('/files', data).then((response) => {
-      var file_name = response.data.fileUrl
-      if(file_name === undefined){
-        file_name = 'files/' + response.data.originalname
-      }
-      this.setState({
-        imageUrl: file_name
-      })
-    })
+   //not in use
   }
     
   render() {
@@ -27,7 +14,6 @@ class Uploader extends Component {
       <div>
         <img width='320' src={this.state.imageUrl} />
         <div>
-          <input type="file" onChange={this.handleUploadFile} />
         </div>  
       </div>
     )
